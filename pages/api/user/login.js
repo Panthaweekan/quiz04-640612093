35509320e8131dd07b36a5ebb81be3e7f5e78309
Panthaweekan_Somngam <tatanoobza1234@hotmail.com>
2022-10-06 15:14:55 +0700

    Finish quiz04

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -5,25 +5,47 @@ import bcrypt from "bcrypt";
 export default function loginRoute(req, res) {
   if (req.method === "POST") {
     const { username, password } = req.body;
+
     //validate body
     if (
       typeof username !== "string" ||
-      username.length === 0 ||
+      username.length <= 0 ||
       typeof password !== "string" ||
-      password.length === 0
-    )
-      return res
-        .status(400)
-        .json({ ok: false, message: "Username or password cannot be empty" });
+      password.length <= 0
+    ) {
+      return res.status(400).json({
+        ok: false,
+        message: "Username or password cannot be empty",
+      });
+    }
 
     const users = readUsersDB();
-    //find user with username & password
 
-    // return res.status(400).json({ ok: false, message: "Invalid Username or Password" });
+    //find users with username, password
+    const foundUser = users.find(
+      (x) => x.username === username && bcrypt.compareSync(password, x.password)
+    );
+    if (!foundUser)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Invalid Username or Password" });
 
     const secret = process.env.JWT_SECRET;
-    //create token and return response
-  } else {
-    return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
+    //sign token
+    const token = jwt.sign(
+      {
+        username: foundUser.username,
+        isAdmin: foundUser.isAdmin,
+      },
+      secret,
+      { expiresIn: "1800s" }
+    );
+    //return response
+    return res.status(200).json({
+      ok: true,
+      username: foundUser.username,
+      isAdmin: foundUser.isAdmin,
+      token: token,
+    });
   }
 }
--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -6,11 +6,18 @@ export default function userRegisterRoute(req, res) {
   if (req.method === "POST") {
     const { username, password, isAdmin } = req.body;
 
-    //check authentication
+    //if we try to add admin account
+    //but token is not admin or token is not attached with request at all
+    //we reject request
     const user = checkToken(req);
-    //return res.status(403).json({ok: false,message: "You do not have permission to create account",});
+    if (!user || !user.isAdmin) {
+      return res.status(403).json({
+        ok: false,
+        message: "You do not have permission to create account",
+      });
+    }
 
-    //validate body
+    //validate body request
     if (
       typeof username !== "string" ||
       username.length === 0 ||
@@ -24,12 +31,23 @@ export default function userRegisterRoute(req, res) {
 
     //check if username is already in database
     const users = readUsersDB();
-    //return res.status(400).json({ ok: false, message: "Username is already taken" });
+    const foundUser = users.find((x) => x.username === username);
+    if (foundUser)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Username is already taken" });
 
-    //create new user and add in db
+    const newUser = {
+      username,
+      password: bcrypt.hashSync(password, 12),
+      isAdmin,
+      money: isAdmin ? null : 0,
+    };
 
+    users.push(newUser);
     writeUsersDB(users);
 
-    //return response
+    //send username back when successfully registered
+    return res.json({ ok: true, username, isAdmin });
   }
 }
